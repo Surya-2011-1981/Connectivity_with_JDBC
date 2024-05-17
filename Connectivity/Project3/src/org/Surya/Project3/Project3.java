ed6ef28655d79a0c013fc6e94a38b37e5d91bdfc
Surya <suryask7549@gmail.com>
2024-05-17 23:58:34 +0530

    Updated

--- Connectivity/Project3/src/org/Surya/Project3/Project3.java
+++ Connectivity/Project3/src/org/Surya/Project3/Project3.java
@@ -1,22 +1,34 @@
-package org.Surya.Package2;
-
+package org.Surya.Project3;
 import java.sql.*;
 
-public class Project2Class_1 {
+public class Project3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Connection con=null;
-		
+		Statement stmt=null;
+		String qry="insert into firstDB.users values(1,'Piyush','80000')";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver class loaded and registered");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");
 			System.out.println("Connection Successfull");
+			stmt=con.createStatement();
+			System.out.print("Statement");
+			stmt.executeUpdate(qry);
 		}catch(ClassNotFoundException | SQLException e)
 		{
 			e.printStackTrace();
 		}finally {
+			if(stmt!=null)
+			{
+				try {
+					stmt.close();
+				}catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
 			if(con!=null)
 			{
 				try {
@@ -26,7 +38,8 @@ public class Project2Class_1 {
 					e.printStackTrace();
 				}
 			}
-			System.out.print("Costly Resources Closed");
+			System.out.println("Data Inserted");
+			System.out.println("Costly Resources Closed");
 		}
 	}
 
---
+++ Connectivity/Project4/src/org/Surya/Package4/ProjectApp.java
@@ -0,0 +1,69 @@
+package org.Surya.Package4;
+
+import java.sql.*;
+
+
+public class ProjectApp {
+
+	public static void main(String[] args) {
+		Connection con=null;
+		Statement st=null;
+//		String query="insert into myapp.app values('Shyam',1,'30')";
+//		String query1="insert into myapp.app values('Ram',2,'34')";
+//		String query2="insert into myapp.app values('Mohan',3,'20')";
+//		String query3="insert into myapp.app values('Bharat',4,'32')";
+//		String query4="insert into myapp.app values('Shyam',5,'39')";
+//		String query5="update myapp.app set Name='Surya' where id=1";
+		String query6="delete from myapp.app where id=3";
+		
+		
+		
+		try {
+			//********  Load and Register Class *******
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Class Loaded and Registered");
+			
+			//******** Setting Up Connection With Database Server  *****
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");
+			System.out.println("Connection Setteled Up ");
+			
+			//******** Creating Statement or Platform (After this the SQL commands will Start working ) *****
+			st=con.createStatement();
+			System.out.println("Platform | Statement  Created ");
+			
+			
+			//******** Executing DB queries  *****
+////			st.executeUpdate(query);
+//			st.executeUpdate(query1);
+//			st.executeUpdate(query2);
+//			st.executeUpdate(query3);
+//			st.executeUpdate(query4);
+			st.executeUpdate(query6);
+			} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(st!=null) {
+				try 
+				{
+					st.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con!=null)
+			{
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Data Updated");
+			System.out.println("Costly resources closed");
+		}
+
+	}
+
+}

2556cb2fcc62c69d20c71484e806461cc6c655ee
Surya <suryask7549@gmail.com>
2024-10-16 23:21:38 +0530

    Third Changes done

--- C_GNIOT/all_operators.c
+++ C_GNIOT/all_operators.c
@@ -3,12 +3,14 @@ int main()
 {
     // printf("%d", (5 < 2 || 3 > 8));
 
+
     // int num = (6 > 7) ? 10 : 20;
     // printf("%d", num);
 
+
     // printf("%d", 1 & 1);
-    printf("%d", 0 ^ 0);
-    printf("%d", 1 | 0);
-    printf("%d", 1 & 1);
+    printf("%d\n", 0 ^ 0);
+    printf("%d\n", 1 | 0);
+    printf("%d\n", 1 & 1);
     return 0;
 }

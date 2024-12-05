c64ab8d2348f06b5445c77c946cd6f1a50500ccc
Surya <suryask7549@gmail.com>
2024-12-05 22:20:41 +0530

    "Updated Q_4.c to implement number swapping and modified DSA_with_java subproject commit status"

---
+++ C_GNIOT/LAB_QUESTIONS/Q_5.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+    printf("Enter Three Numbers : ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            printf("%d is the largest number", num1);
+        }
+        else
+        {
+            printf("%d is the Largest number ", num3);
+        }
+    }
+    else if (num2 > num1)
+    {
+        if (num2 > num3)
+        {
+            printf("%d is the largest number", num2);
+        }
+        else
+        {
+            printf("%d is the Largest number ", num3);
+        }
+    }
+    else if (num3 > num1)
+    {
+        if (num3 > num2)
+        {
+            printf("%d is the largest number", num3);
+        }
+        else
+        {
+            printf("%d is the Largest number ", num2);
+        }
+    }
+    else if (num1 == num2 || num1 == num3 || num2 == num3)
+    {
+        if (num1 == num2)
+        {
+            if (num1 > num3)
+            {
+                printf("%d is the largest numbers", num1);
+            }
+            else
+            {
+                printf("%d is the largest numbers", num3);
+            }
+        }
+        else if (num1 == num3)
+        {
+            if (num1 > num2)
+            {
+                printf("%d is the largest numbers", num1);
+            }
+            else
+            {
+                printf("%d is the largest numbers", num2);
+            }
+        }
+        else if (num2 == num3)
+        {
+            if (num2 > num1)
+            {
+                printf("%d is the largest numbers", num2);
+            }
+            else
+            {
+                printf("%d is the largest numbers", num1);
+            }
+        }
+    }
+    else
+    {
+        printf("All numbers are equal");
+    }
+    return 0;
+}

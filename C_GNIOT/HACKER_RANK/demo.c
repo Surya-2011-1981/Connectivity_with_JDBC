a25a334bfa1bcfb53fc01c3231b9e5e27bab8bb3
Surya <suryask7549@gmail.com>
2025-01-05 23:59:09 +0530

    HackerRankk Pattern 1

--- C_GNIOT/HACKER_RANK/demo.c
+++ C_GNIOT/HACKER_RANK/demo.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-int main()
-{
-    int i = 5;
-    while (i--)
-        printf("%d , ", i);
+
+void printMatrix(int n) {
+    int size = 2 * n - 1; // Size of the matrix
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int minDistance = i; 
+            if (j < minDistance) {
+                minDistance = j;
+            }
+            if (size - i - 1 < minDistance) {
+                minDistance = size - i - 1;
+            }
+            if (size - j - 1 < minDistance) {
+                minDistance = size - j - 1;
+            }
+            printf("%d ", n - minDistance);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter a value for n: ");
+    scanf("%d", &n);
+    printMatrix(n);
     return 0;
 }
